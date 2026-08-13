da3ad20762d5a65b6b94ac291bbe99c90e07e48c
fortunateatworekire <fortunateatworekire@gmail.com>
2026-08-13 04:20:00 +0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -4,7 +4,14 @@ import joblib
 
 
 # Load the trained model and column names
-model = joblib.load("../models/rf_model.pkl")
+# model = joblib.load("../models/rf_model.pkl")
+import os
+import joblib
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+model_path = os.path.join(BASE_DIR, "models", "rf_model.pkl")
+
+model = joblib.load(model_path)
 model_columns = joblib.load("../models/model_columns.pkl")
 
 
